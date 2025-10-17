36bf170e3244d17d95b895d3042ecefb9b1a3ee3
syed-khamarudheen <techkamar@gmail.com>
2025-10-17 16:40:30 +0530

    Login Page Ready

--- odiyan-ui/src/pages/login/login.jsx
+++ odiyan-ui/src/pages/login/login.jsx
@@ -1,7 +1,21 @@
+import './login.css';
+
 const Login = () => {
     return(
         <>
-            <div style={{color:'white'}}>This is Login Page</div>
+            <div className='login-root-container'>
+                <div className='login-box-container'>
+                    <div className='login-entry'>
+                        <input type='text' placeholder='USERNAME'/>
+                        <input type='password' placeholder='PASSWORD'/>
+                        <button className='scale-btn crimson'>LOGIN</button>
+                    </div>
+                    <div className='or-text-container'>---- OR ----</div>
+                    <div className='login-entry'>
+                        <button className='scale-btn green'>REGISTER</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

c62cf071ce91f6266d17735a77efa13c212bd138
syed-khamarudheen <techkamar@gmail.com>
2025-10-17 16:15:01 +0530

    Auth Check for Home Component On Load working Auth Me API also working

--- odiyan-ui/src/pages/home/home.jsx
+++ odiyan-ui/src/pages/home/home.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+
 const Home = () => {
+    const checkLogin = async() => {
+        let response = await("/api/auth/me")
+        if(response.status_code!=200){
+            window.location.href="/login";
+        }
+    }
+    useEffect(()=>{
+        checkLogin();
+    },[]);
+    
     return (
         <>This is Home Page</>
     )

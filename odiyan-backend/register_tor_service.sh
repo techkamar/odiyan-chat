d487aa5ad576c0742c999daebc24c9f49335c048
techkamar <techkamar@gmail.com>
2025-10-18 19:10:38 +0530

    Added Proper prints.

--- odiyan-backend/register_tor_service.sh
+++ odiyan-backend/register_tor_service.sh
@@ -5,16 +5,21 @@
 #
 
 # Move to right folder
+echo "⏳ Moving to /etc/tor directory"
 cd /etc/tor
 
 # Add Hidden Service
+echo "⏳ Adding Hidden Service Configuration"
 echo "HiddenServiceDir /var/lib/tor/hidden_service/" >> torrc
 echo "HiddenServicePort 80 127.0.0.1:8000" >> torrc
 
 # Restart TOR service
+echo "⏳ Restarting TOR Service"
 systemctl restart tor
 
 # Sleep for 3 seconds and display the .onion domain name
+echo "⏳ Fetching .onion domain name"
 sleep 3
 clear
+echo "✅ Displaying TOR Domain Name"
 cat /var/lib/tor/hidden_service/hostname
--- odiyan-backend/tor_start.sh
+++ odiyan-backend/tor_start.sh
@@ -1,17 +1,23 @@
 # Refresh Repo
+echo "⏳ Refreshing debian repos"
 sudo apt update -y
 
 # Install Python venv
-sudo apt install python3-venv
+echo "⏳ Installing Python3 VENV module"
+sudo apt install python3-venv -y
 
 # Create new python environment
+echo "⏳ Creating new Python3 environment"
 python3 -m venv venv
 
 #Activate ENVIRONMENT
+echo "⏳ Activating Python3 environment"
 source venv/bin/activate
 
 # Install PIP packages
+echo "⏳ Installing PIP packages"
 torsocks pip install -r requirements.txt
 
 # Start server with Uvicorn
+echo "⏳ Starting UVICORN server"
 uvicorn main:app --port 8000 --reload

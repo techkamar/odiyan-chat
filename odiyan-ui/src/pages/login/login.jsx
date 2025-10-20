e3f98738e9964ba30279f2435166d20eb7b616dc
syed-khamarudheen <techkamar@gmail.com>
2025-10-20 11:50:06 +0530

    State Managed for Login and Register Component

--- odiyan-ui/src/pages/login/login.jsx
+++ odiyan-ui/src/pages/login/login.jsx
@@ -2,15 +2,21 @@ import { useState } from 'react';
 import './login.css';
 
 const Login = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const doLogin = () => {
+        alert("Username : "+username+" and Password : "+password);
+    }
     return(
         <>
             <div className='login-root-container'>
                 <div className='login-box-container'>
                     <div className='login-entry'>
                         <p style={{textAlign:'center', fontWeight:'bold', fontSize:'20px'}}> Login to your account</p>
-                        <input type='text' placeholder='USERNAME'/>
-                        <input type='password' placeholder='PASSWORD'/>
-                        <button className='scale-btn'>LOGIN</button>
+                        <input type='text' placeholder='USERNAME' onChange={(e)=>(setUsername(e.target.value))}/>
+                        <input type='password' placeholder='PASSWORD'onChange={(e)=>(setPassword(e.target.value))}/>
+                        <button className='scale-btn'onClick={()=>(doLogin())}>LOGIN</button>
                     </div>
                     <div className='or-text-container'>---- OR ----</div>
                     <div className='login-entry'>
@@ -23,16 +29,23 @@ const Login = (props) => {
 }
 
 const Register = (props) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    
+    const doRegister = () => {
+        alert("Username : "+username+" , Password : "+password+ " and Confirm Password : "+confirmPassword);
+    }
     return(
         <>
             <div className='login-root-container'>
                 <div className='login-box-container'>
                     <div className='login-entry'>
                         <p style={{textAlign:'center', fontWeight:'bold', fontSize:'20px'}}> Create a new account </p>
-                        <input type='text' placeholder='USERNAME'/>
-                        <input type='password' placeholder='PASSWORD'/>
-                        <input type='password' placeholder='CONFIRM PASSWORD'/>
-                        <button className='scale-btn'>REGISTER</button>
+                        <input type='text' placeholder='USERNAME'onChange={(e)=>(setUsername(e.target.value))}/>
+                        <input type='password' placeholder='PASSWORD' onChange={(e)=>(setPassword(e.target.value))}/>
+                        <input type='password' placeholder='CONFIRM PASSWORD'onChange={(e)=>(setConfirmPassword(e.target.value))}/>
+                        <button className='scale-btn' onClick={()=>(doRegister())}>REGISTER</button>
                     </div>
                     <div className='or-text-container'>---- OR ----</div>
                     <div className='login-entry'>

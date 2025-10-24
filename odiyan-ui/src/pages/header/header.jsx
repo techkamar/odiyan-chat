af917b1a88a2f1728d3257a070919853f8cc481f
syed-khamarudheen <techkamar@gmail.com>
2025-10-24 17:37:03 +0530

    Self Destruct working with Confirmation Dialog

--- odiyan-ui/src/pages/header/header.jsx
+++ odiyan-ui/src/pages/header/header.jsx
@@ -12,6 +12,16 @@ const HeaderBanner = (props) => {
         window.location.href="/login";
     }
 
+    const selfDestruct = async() => {
+        const performDelete = confirm("( ! ) Do you want to delete your account along with all data??")
+        if(performDelete){
+            let response = await fetch("/api/user/self-destruct",{method:'DELETE'});
+            if(response.status==200){
+                window.location.href="/login";
+            }
+        }
+    }
+
 
     return (
         <>
@@ -19,7 +29,7 @@ const HeaderBanner = (props) => {
                 <div className="header-banner-left-container">Odiyan Chat v0.1 [{props.username}]</div>
                 <div className="header-banner-right-container">
                     <button className='header-btn' onClick={()=>(setShowAddNewMesasgeBox(true))}>[+] New Message</button>
-                    <button className='header-btn'>Self Destruct Me!</button>
+                    <button className='header-btn' onClick={()=>(selfDestruct())}>Self Destruct Me!</button>
                     <button className='header-btn' onClick={()=>(logout())}>Logout</button>
                 </div>
             </div>
